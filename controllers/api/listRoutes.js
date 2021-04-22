7b7dea6ea91df976147856a3f5c8b028a54ae7b5
G Code <seanlfrancis@gmail.com>
2021-04-22 13:03:12 -0500

    Just formatted listRoutes

--- controllers/api/listRoutes.js
+++ controllers/api/listRoutes.js
@@ -1,16 +1,16 @@
 const router = require('express').Router();
-const {ShoppingList} = require('../../models');
+const { ShoppingList } = require('../../models');
 const withAuth = require('../../utils/auth');
 
 
 router.post('/', async (req, res) => {
-    try{
+    try {
         const addItem = await ShoppingList.create({
             ...req.body,
             user_id: req.session.user_id,
             item_name: req.session.item_name,
             price: req.session.price,
-           
+
         });
         res.status(200).json(addItem);
     } catch (err) {
